e76685efbaa851ede1e1510a037e4f03b86c2e69
Quyet <quyet@magingam.com>
2015-09-24 09:24:19 +0700

    Change Config param

--- protected/config/param.php
+++ protected/config/param.php
@@ -1,10 +1,12 @@
 <?php
-return array(
-    'default_role' => array('superuser','administrator','moderator','publisher','author','member','guest'),
-    'default_role_title' => array('Super User','Administrator','Moderator','Publisher & Author','Member'),
+
+$param = array(
+    'default_role' => array('superuser', 'administrator', 'moderator', 'publisher', 'author', 'member', 'guest'),
+    'default_role_title' => array('Super User', 'Administrator', 'Moderator', 'Publisher & Author', 'Member'),
     'display_role' => array(
-        0 => array(0,1,2,3,4),
-        1 => array(1,2,3,4),
+        0 => array(0, 1, 2, 3, 4),
+        1 => array(1, 2, 3, 4),
         2 => array(3),
     )
 );
+return CMap::mergeArray(require(dirname(__FILE__) . '/param_local.php'), $param);
